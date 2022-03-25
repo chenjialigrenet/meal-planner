785ac45cfd77834f7bc1b02898f84d1a50471f43
Chenjia Li <cligrenet@gmail.com>
2022-03-25 11:58:02 +0100

    Image uploader in progress

---
+++ frontend/src/components/ImageUploader.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import useFormFields from '../lib/hooksLib';
+
+function ImageUploader() {
+	const [fields, handleFieldChange] = useFormFields({ photo: '' });
+	// const [image, setImage] = useState();
+	const [imageURL, setImageURL] = useState();
+
+	useEffect(() => {
+		if (!fields.photo) return;
+		const newImageUrl = URL.createObjectURL(fields.photo);
+		setImageURL(newImageUrl);
+	}, []);
+
+	// TODO modify handleFieldChange function in hooksLib
+
+	return (
+		<>
+			<Form.Group controlId="photo">
+				<Form.Label>Image</Form.Label>
+				<Form.Control
+					type="file"
+					multiple={false}
+					accept="image/*"
+					value={fields.photo}
+					onChange={handleFieldChange}
+				/>
+				{imageURL && (
+					<img src={imageURL} alt="recipe_photo" height={100} />
+				)}
+			</Form.Group>
+		</>
+	);
+}
+
+export default ImageUploader;
