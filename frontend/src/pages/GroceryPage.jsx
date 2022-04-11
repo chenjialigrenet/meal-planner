d9e06b6c01642935d9de22aff2f7e779f4bf2281
Chenjia Li <cligrenet@gmail.com>
2022-04-11 15:14:24 +0200

    Start working on grocery page

--- frontend/src/pages/GroceryPage.jsx
+++ frontend/src/pages/GroceryPage.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import axiosInstance from '../axiosApi';
+import { useAppContext } from '../lib/contextLib';
+import { v4 as uuidv4 } from 'uuid';
+import { Card } from 'react-bootstrap';
 import './GroceryPage.css';
 
 function GroceryPage() {
+	const [recipes, setRecipes] = useState([]);
+	// Get active plan from current user data
+	const { currentUser } = useAppContext();
+	const activePlanId = currentUser.active_plan;
+
+	// Fetch active plan
+	const fetchActivePlan = async () => {
+		try {
+			const response = await axiosInstance.get(`/plans/${activePlanId}`);
+			// console.log('ACTIVE PLAN', response.data);
+			setRecipes(recipesOfTheDay(response.data));
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	useEffect(() => {
+		fetchActivePlan();
+	}, []);
+
+	// Prepare for active table row
+	const currentdate = new Date();
+	let currentWeekDay = currentdate.getDay();
+
+	// Get recipesOfTheDay
+	const recipesOfTheDay = (plan) => {
+		const recipes = [];
+		plan.meals.forEach((planDay) => {
+			if (planDay.day === currentWeekDay) {
+				planDay.recipes.forEach((recipe) => recipes.push(recipe));
+			}
+		});
+		console.log(recipes);
+		return recipes;
+	};
+
 	return (
 		<div className="GroceryPage">
-			<h3>Grocery List</h3>
-			<p>All ingredients + quantity</p>
+			<div className="grocery-day">
+				<h4>Grocery of the day</h4>
+				{recipes.map((recipe) => {
+					return (
+						<Card
+							className="grocery-day-recipe-card"
+							key={uuidv4()}
+						>
+							<Card.Header>
+								<b>{recipe.title}</b>
+							</Card.Header>
+							<Card.Body>
+								{recipe.recipe_ingredients.map((recipe_ing) => {
+									return (
+										<div key={uuidv4()}>
+											{recipe_ing.ingredient.name}:{' '}
+											{recipe_ing.quantity}{' '}
+											{recipe_ing.ingredient.unit}
+										</div>
+									);
+								})}
+							</Card.Body>
+						</Card>
+					);
+				})}
+			</div>
+			<div className="grocery-week">
+				<h4>Grocery of the week</h4>
+			</div>
 		</div>
 	);
 }
