74fb35f31fcc169344cf453d5a8d786e6a5d16d5
Chenjia Li <cligrenet@gmail.com>
2022-06-30 08:34:05 +0200

    On RecipeForm, move fetchAllIngredients in useEffect hook, update dependencies, clean comments

--- frontend/src/components/forms/RecipeForm.jsx
+++ frontend/src/components/forms/RecipeForm.jsx
@@ -3,122 +3,95 @@ import { useNavigate, useParams } from 'react-router-dom';
 import useFormFields from '../../lib/hooksLib';
 import LoaderButton from '../utilities/LoaderButton';
 import axiosInstance from '../../axiosApi';
-import onError from '../../lib/errorLib';
 import { Col, Row, Table, Form } from 'react-bootstrap';
 import Select from 'react-select';
 import { FaTimes } from 'react-icons/fa';
 import './RecipeForm.css';
-// import ImageUploader from './ImageUploader';
-
-// {
-//     name: "recipe",
-//     ingredients: [{ ingredient_id: 3, quantity: 5 }]
-// }
-
-// 1/ create Recipe without ingredients => recipe.id
-// 2/ for ingredient_attributes in ingredients:
-//       ingredient = find Ingredient with ingredient_attributes["ingredient_id"]
-//       create RecipeIngredient ingredient=ingredient, recipe=recipe, quantity=ingredient_attributes["quantity"]
 
 function RecipeForm() {
 	const params = useParams();
 	const isCreate = !params.recipeId;
-
 	// Axios fetching recipe data
 	const [isFetching, setIsFetching] = useState(true);
 	// Spinner on the submit button
 	const [isLoading, setIsLoading] = useState(false);
 	// Redirect
 	const navigate = useNavigate();
-
 	// GET all ingredients + React Select options
 	const [ingredients, setIngredients] = useState([]);
-	const fetchAllIngredients = async () => {
-		setIsFetching(true);
-		try {
-			const response = await axiosInstance.get('/ingredients/');
-			// console.log(response.data);
-			setIngredients(response.data);
-			setIsFetching(false);
-		} catch (err) {
-			console.log(err);
-			setIsFetching(false);
-		}
-	};
-
-	// GET one recipe
-	const fetchRecipe = async () => {
-		setIsFetching(true);
-		try {
-			const response = await axiosInstance.get(
-				`/recipes/${params.recipeId}/`
-			);
-			const recipeData = response.data;
-			// Prepare a current photo backup for later file upload current photo preview
-			recipeData.photo_current = recipeData.photo;
-			recipeData.photo = '';
-			setFieldsValues(recipeData);
-			setIsFetching(false);
-		} catch (err) {
-			console.log(err);
-			setIsFetching(false);
-		}
-	};
+	//Custom hook
+	const [fields, handleFieldChange, changeFieldValue, setFieldsValues] = useFormFields({
+		id: null,
+		title: '',
+		summary: '',
+		serves: '',
+		cooking_temperature: '',
+		cooking_time: '',
+		prep_time: '',
+		recipe_ingredients: [],
+		instructions: '',
+		photo: '',
+		creation_date: '',
+		difficulty: '1',
+	});
 
 	useEffect(() => {
+		const fetchAllIngredients = async () => {
+			setIsFetching(true);
+			try {
+				const response = await axiosInstance.get('/ingredients/');
+
+				setIngredients(response.data);
+				setIsFetching(false);
+			} catch (err) {
+				console.log(err);
+				setIsFetching(false);
+			}
+		};
+
 		fetchAllIngredients();
+
+		// GET one recipe
+		const fetchRecipe = async () => {
+			setIsFetching(true);
+			try {
+				const response = await axiosInstance.get(`/recipes/${params.recipeId}/`);
+				const recipeData = response.data;
+				// Prepare a current photo backup for later file upload current photo preview
+				recipeData.photo_current = recipeData.photo;
+				recipeData.photo = '';
+
+				setFieldsValues(recipeData);
+				setIsFetching(false);
+			} catch (err) {
+				console.log(err);
+				setIsFetching(false);
+			}
+		};
+
 		if (!isCreate) {
 			fetchRecipe();
 		}
-	}, []);
-
-	const [fields, handleFieldChange, changeFieldValue, setFieldsValues] =
-		useFormFields({
-			id: null,
-			title: '',
-			summary: '',
-			serves: '',
-			cooking_temperature: '',
-			cooking_time: '',
-			prep_time: '',
-			recipe_ingredients: [],
-			instructions: '',
-			photo: '',
-			creation_date: '',
-			difficulty: '1',
-		});
+	}, [isCreate, params.recipeId, setFieldsValues]);
 
 	// React Select
 	ingredients.map((ing) => {
-		return (
-			(ing.value = ing.name),
-			(ing.label = ing.name[0].toUpperCase() + ing.name.substring(1))
-		);
+		return (ing.value = ing.name), (ing.label = ing.name[0].toUpperCase() + ing.name.substring(1));
 	});
 	const ingredient_options = ingredients;
-	// console.log(ingredient_options);
 
-	// const [selectedIngredients, setSelectedIngredients] = useState([]);
 	const handleAddIngredient = (selectedIngredient) => {
 		changeFieldValue(
 			'recipe_ingredients',
-			fields.recipe_ingredients.concat([
-				{ ingredient: selectedIngredient, quantity: 1 },
-			])
+			fields.recipe_ingredients.concat([{ ingredient: selectedIngredient, quantity: 1 }]),
 		);
 	};
-	// console.log(selectedIngredients);
 
-	const updateRecipeIngredientQuantity = (
-		recipeIngredientId,
-		newQuantity
-	) => {
+	const updateRecipeIngredientQuantity = (recipeIngredientId, newQuantity) => {
 		const recipeIngredient = fields.recipe_ingredients.find(
-			// (recipeIngredient) => recipeIngredient.id === recipeIngredientId
-			(recipeIngredient) =>
-				recipeIngredient.ingredient.id === recipeIngredientId
+			(recipeIngredient) => recipeIngredient.ingredient.id === recipeIngredientId,
 		);
-		console.log(newQuantity);
+
 		recipeIngredient.quantity = parseInt(newQuantity);
 		if (isNaN(recipeIngredient.quantity)) {
 			recipeIngredient.quantity = '';
@@ -130,25 +103,27 @@ function RecipeForm() {
 		if (window.confirm('Are you sure to delete this ingredient?')) {
 			changeFieldValue(
 				'recipe_ingredients',
-				fields.recipe_ingredients.filter(
-					(recipeIngredient) => recipeIngredient.ingredient.id !== id
-				)
+				fields.recipe_ingredients.filter((recipeIngredient) => recipeIngredient.ingredient.id !== id),
 			);
 		}
 	};
 
-	// const validateForm = () => {
-	// 	return (
-	// 		fields.title.length > 0 &&
-	// 		fields.summary.length > 0 &&
-	// 		fields.serves.length > 0 &&
-	// 		fields.cooking_temperature.length > 0 &&
-	// 		fields.cooking_time.length > 0 &&
-	// 		fields.prep_time.length > 0 &&
-	// 		fields.recipe_ingredients.length > 0 &&
-	// 		fields.instructions.length > 0
-	// 	);
-	// };
+	// TODO
+	//const validateForm = () => {
+	// return (
+	// 	fields.title.length > 0 &&
+	// 	fields.summary.length > 0 &&
+	// 	fields.serves.length > 0 &&
+	// 	+fields.serves > 0 &&
+	// 	fields.cooking_temperature.length > 0 &&
+	// 	fields.cooking_time.length > 0 &&
+	// 	+fields.cooking_time > 0 &&
+	// 	fields.prep_time.length > 0 &&
+	// 	+fields.prep_time > 0 &&
+	// 	fields.recipe_ingredients.length > 0 &&
+	// 	fields.instructions.length > 0
+	// );
+	//};
 
 	// CREATE one recipe
 	const handleCreateRecipe = async (event) => {
@@ -164,16 +139,6 @@ function RecipeForm() {
 			formData.append('cooking_temperature', fields.cooking_temperature);
 			formData.append('cooking_time', fields.cooking_time);
 			formData.append('prep_time', fields.prep_time);
-			// fields.recipe_ingredients.forEach((recipeIngredient, index) => {
-			// 	formData.append(
-			// 		`recipe_ingredients[${index}][ingredient]`,
-			// 		recipeIngredient.ingredient.id
-			// 	);
-			// 	formData.append(
-			// 		`recipe_ingredients[${index}][quantity]`,
-			// 		recipeIngredient.quantity
-			// 	);
-			// });
 			formData.append(
 				'recipe_ingredients',
 				JSON.stringify(
@@ -182,11 +147,13 @@ function RecipeForm() {
 							ingredient: recipeIngredient.ingredient.id,
 							quantity: recipeIngredient.quantity,
 						};
-					})
-				)
+					}),
+				),
 			);
 			formData.append('instructions', fields.instructions);
-			formData.append('photo', fields.photo, fields.photo.name);
+			if (fields.photo) {
+				formData.append('photo', fields.photo, fields.photo.name);
+			}
 			formData.append('difficulty', fields.difficulty);
 
 			await axiosInstance.post('/recipes/', formData, {
@@ -197,9 +164,8 @@ function RecipeForm() {
 
 			setIsLoading(false);
 			navigate('/recipe');
-			// window.location.reload();
 		} catch (err) {
-			onError(err);
+			console.log(err);
 			setIsLoading(false);
 		}
 	};
@@ -231,8 +197,8 @@ function RecipeForm() {
 						} else {
 							//TODO
 						}
-					})
-				)
+					}),
+				),
 			);
 			formData.append('instructions', fields.instructions);
 			if (fields.photo) {
@@ -247,7 +213,6 @@ function RecipeForm() {
 			});
 			setIsLoading(false);
 			navigate(`/recipes/${params.recipeId}`);
-			// window.location.href = '/recipe';
 		} catch (err) {
 			console.log(err);
 			setIsLoading(false);
@@ -256,19 +221,9 @@ function RecipeForm() {
 
 	return (
 		<div className="RecipeForm">
-			<h3>
-				{isCreate === false
-					? `Update Recipe (ID: ${params.recipeId})`
-					: 'Create Recipe'}
-			</h3>
+			<h3>{isCreate === false ? `Update Recipe (ID: ${params.recipeId})` : 'Create Recipe'}</h3>
 			{!isFetching && (
-				<Form
-					onSubmit={
-						isCreate === false
-							? handleUpdateRecipe
-							: handleCreateRecipe
-					}
-				>
+				<Form onSubmit={isCreate === false ? handleUpdateRecipe : handleCreateRecipe}>
 					<Form.Group controlId="title">
 						<Form.Label>Title</Form.Label>
 						<Form.Control
@@ -300,9 +255,7 @@ function RecipeForm() {
 							/>
 						</Form.Group>
 						<Form.Group as={Col} controlId="cooking_temperature">
-							<Form.Label>
-								Cooking temperature (in °C){' '}
-							</Form.Label>
+							<Form.Label>Cooking temperature (in °C) </Form.Label>
 							<Form.Control
 								type="number"
 								value={fields.cooking_temperature}
@@ -335,42 +288,30 @@ function RecipeForm() {
 						<Form.Label>Ingredients</Form.Label>
 						{isCreate === false ? (
 							<ul>
-								{fields.recipe_ingredients.map(
-									(recipeIngredient) => {
-										return (
-											<li key={recipeIngredient.id}>
-												<Row>
-													<Col>
-														<Form.Control
-															value={
-																recipeIngredient.quantity
-															}
-															onChange={(event) =>
-																updateRecipeIngredientQuantity(
-																	recipeIngredient.id,
-																	event.target
-																		.value
-																)
-															}
-															name="quantity"
-														/>
-													</Col>
-													<Col>
-														{
-															recipeIngredient
-																.ingredient.unit
-														}{' '}
-														of{' '}
-														{
-															recipeIngredient
-																.ingredient.name
+								{fields.recipe_ingredients.map((recipeIngredient) => {
+									return (
+										<li key={recipeIngredient.id}>
+											<Row>
+												<Col>
+													<Form.Control
+														value={recipeIngredient.quantity}
+														onChange={(event) =>
+															updateRecipeIngredientQuantity(
+																recipeIngredient.id,
+																event.target.value,
+															)
 														}
-													</Col>
-												</Row>
-											</li>
-										);
-									}
-								)}
+														name="quantity"
+													/>
+												</Col>
+												<Col>
+													{recipeIngredient.ingredient.unit} of{' '}
+													{recipeIngredient.ingredient.name}
+												</Col>
+											</Row>
+										</li>
+									);
+								})}
 							</ul>
 						) : (
 							<Select
@@ -392,57 +333,35 @@ function RecipeForm() {
 								</tr>
 							</thead>
 							<tbody>
-								{fields.recipe_ingredients.map(
-									(recipeIngredient) => (
-										<tr
-											key={recipeIngredient.ingredient.id}
-										>
-											<td>
-												{
-													recipeIngredient.ingredient
-														.label
+								{fields.recipe_ingredients.map((recipeIngredient) => (
+									<tr key={recipeIngredient.ingredient.id}>
+										<td>{recipeIngredient.ingredient.label}</td>
+										<td>{recipeIngredient.ingredient.unit}</td>
+										<td>
+											<input
+												id="quantity"
+												name="quantity"
+												type="number"
+												onInput={(event) =>
+													updateRecipeIngredientQuantity(
+														recipeIngredient.ingredient.id,
+														event.target.value,
+													)
 												}
-											</td>
-											<td>
-												{
-													recipeIngredient.ingredient
-														.unit
-												}
-											</td>
-											<td>
-												<input
-													id="quantity"
-													name="quantity"
-													type="number"
-													onInput={(event) =>
-														updateRecipeIngredientQuantity(
-															recipeIngredient
-																.ingredient.id,
-															event.target.value
-														)
-													}
-													value={
-														recipeIngredient.quantity
-													}
-												/>
-											</td>
-											<td>
-												<FaTimes
-													onClick={() =>
-														handleDeleteIngredient(
-															recipeIngredient
-																.ingredient.id
-														)
-													}
-												/>
-											</td>
-										</tr>
-									)
-								)}
+												value={recipeIngredient.quantity}
+											/>
+										</td>
+										<td>
+											<FaTimes
+												onClick={() => handleDeleteIngredient(recipeIngredient.ingredient.id)}
+											/>
+										</td>
+									</tr>
+								))}
 							</tbody>
 						</Table>
 					)}
-					{/* TODO add editor */}
+
 					<Form.Group controlId="instructions">
 						<Form.Label>Instructions</Form.Label>
 						<Form.Control
@@ -453,8 +372,6 @@ function RecipeForm() {
 							name="instructions"
 						/>
 					</Form.Group>
-					{/* TODO */}
-					{/* <ImageUploader /> */}
 
 					<Form.Group controlId="photo">
 						<Form.Label>Image</Form.Label>
@@ -467,11 +384,7 @@ function RecipeForm() {
 						></input>
 						{(fields.photo || fields.photo_current) && (
 							<img
-								src={
-									fields.photo
-										? URL.createObjectURL(fields.photo)
-										: fields.photo_current
-								}
+								src={fields.photo ? URL.createObjectURL(fields.photo) : fields.photo_current}
 								alt="recipe"
 								style={{ marginTop: '1em', width: '5em' }}
 							/>
@@ -480,11 +393,7 @@ function RecipeForm() {
 
 					<Form.Group controlId="difficulty">
 						<Form.Label>Difficulty</Form.Label>
-						<Form.Select
-							value={fields.difficulty}
-							onChange={handleFieldChange}
-							name="difficulty"
-						>
+						<Form.Select value={fields.difficulty} onChange={handleFieldChange} name="difficulty">
 							<option value="1">Beginner</option>
 							<option value="2">Easy</option>
 							<option value="3">Normal</option>
